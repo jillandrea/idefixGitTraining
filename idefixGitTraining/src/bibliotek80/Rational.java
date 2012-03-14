a03cfb679eb214d0a49c0480cec8d81e7d8652da
Rasmus Wollesen <tempusmane@ofir.dk>
2012-03-14 09:28:05 +0100

    Lavet skelettet

---
+++ idefixGitTraining/src/bibliotek80/Rational.java
@@ -0,0 +1,63 @@
+package bibliotek80;
+
+public class Rational {
+
+	public final int MINUS1 = 1;
+	
+	private int numerator;
+	private int denominator;
+	
+	public Rational (){
+		
+	}
+	
+	public Rational add(){
+		return null;
+		
+	}
+	
+	public Rational sub(){
+		return null;
+		
+	}
+	
+	public Rational mul(){
+		
+		return null;
+	}
+	
+	public Rational div(){
+		
+		return null;
+	}
+	
+	public void invert(){
+		
+	}
+	
+	public void negate(){
+		
+	}
+	
+	public Rational reduce(){
+		return null;
+		
+	}
+	
+	public double toDouble(){
+		
+		return 0;
+	}
+	
+	public String toString()
+	{
+		return "";
+	}
+	
+	public int gcd(int a, int b){
+		
+		return 0;
+	}
+	
+	
+}
