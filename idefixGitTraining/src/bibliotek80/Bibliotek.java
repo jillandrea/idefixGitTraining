643f022c3234efe7d16f0512f448b91829455382
Nur <Nur@10.77.35.86>
2012-03-14 10:02:05 +0100

    Committing the balls

--- idefixGitTraining/src/bibliotek80/Bibliotek.java
+++ idefixGitTraining/src/bibliotek80/Bibliotek.java
@@ -2,12 +2,69 @@ package bibliotek80;
 
 public class Bibliotek {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println("Your face!!!");
+public static enum BibType {
+	HOVEDBIBLIOTEK, AFDELINGSBIBLIOTEK;
+	public String toString() {
+		switch(this) {
+		case AFDELINGSBIBLIOTEK:
+			return "Afdelingsbibliotek";
+		default:
+			return "Hovebibliotek";
+		}
 	}
+}
+	
+private BogEksemplar bogBestand[];
+private Person laanere[];
+private Ansat ansatte[];
+private String navn;
+private BibType type;
 
+public Bibliotek() {
+	
+}
+
+public Bibliotek(BogEksemplar bogBestand[], Person laanere[], Ansat ansatte[], String navn, BibType type) {
+	this.bogBestand = bogBestand;
+	this.laanere = laanere;
+	this.ansatte = ansatte;
+	this.navn = navn;
+	this.type = type;
+}
+
+public void addBog(BogEksemplar bog) {
+	bogBestand[0] = bog;
+}
+
+public void addAnsat(Ansat ansat) {
+	ansatte[0] = ansat;
+}
+
+public void addLaaner(Person laaner) {
+	laanere[0] = laaner;
+}
+
+public void udskrivBogListe() {
+	for(int i = 0; i < bogBestand.length; i++) {
+		System.out.println(bogBestand[i]);
+	}
+}
+
+public void udskrivLaanere() {
+	for(int i = 0; i < laanere.length; i++) {
+		System.out.println(laanere[i]);
+	}
+}
+
+public void udskrivAnsatte() {
+	for(int i = 0; i < ansatte.length; i++) {
+		System.out.println(ansatte[i]);
+	
+}
+
+public String toString() {
+	String s = this.type.toString();
+	return s;
+}
+	
 }
