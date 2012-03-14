4feb69f70c3226ab70fb6e25dbfb4640613be7bd
Nur <Nur@10.77.35.86>
2012-03-14 10:13:23 +0100

    Final balls

--- idefixGitTraining/src/bibliotek80/Bibliotek.java
+++ idefixGitTraining/src/bibliotek80/Bibliotek.java
@@ -1,7 +1,16 @@
 package bibliotek80;
 
-public class Bibliotek {
+/**
+ *  @author Nurudin, Glenn, Martin, Janus
+ *
+ */
 
+public class Bibliotek {
+/**
+ * Her opretter vi vores statiske klasse kaldet BibType,
+ * som skal definere type af bibliotek.
+ *
+ */
 public static enum BibType {
 	HOVEDBIBLIOTEK, AFDELINGSBIBLIOTEK;
 	public String toString() {
@@ -20,10 +29,22 @@ private Ansat ansatte[];
 private String navn;
 private BibType type;
 
+
+/**
+ * Tom constructor
+ */
 public Bibliotek() {
 	
 }
 
+/**
+ * Constructor til oprettelse af objekt
+ * @param bogBestand
+ * @param laanere
+ * @param ansatte
+ * @param navn
+ * @param type
+ */
 public Bibliotek(BogEksemplar bogBestand[], Person laanere[], Ansat ansatte[], String navn, BibType type) {
 	this.bogBestand = bogBestand;
 	this.laanere = laanere;
@@ -32,36 +53,60 @@ public Bibliotek(BogEksemplar bogBestand[], Person laanere[], Ansat ansatte[], S
 	this.type = type;
 }
 
+/**
+ * Metode der tilføjer en bog til bogBestand arrayet
+ * @param bog
+ */
 public void addBog(BogEksemplar bog) {
 	bogBestand[0] = bog;
 }
 
+/**
+ * Metode der tilføjer en ansat til ansatte arrayet
+ * @param ansat
+ */
 public void addAnsat(Ansat ansat) {
 	ansatte[0] = ansat;
 }
 
+/**
+ * Metode der tilføjer en laaner til laanere arrayet
+ * @param laaner
+ */
 public void addLaaner(Person laaner) {
 	laanere[0] = laaner;
 }
 
+/**
+ * Metode der uskriver hele bogBestand arrayet
+ */
 public void udskrivBogListe() {
 	for(int i = 0; i < bogBestand.length; i++) {
 		System.out.println(bogBestand[i]);
 	}
 }
 
+/**
+ * Metode der udskriver laanere arrayet
+ */
 public void udskrivLaanere() {
 	for(int i = 0; i < laanere.length; i++) {
 		System.out.println(laanere[i]);
 	}
 }
 
+/**
+ * Metode der udskriver ansatte arrayet
+ */
 public void udskrivAnsatte() {
 	for(int i = 0; i < ansatte.length; i++) {
 		System.out.println(ansatte[i]);
 	}
 }
 
+/**
+ * toString metode der udskriver typen af bibliotek
+ */
 public String toString() {
 	String s = this.type.toString();
 	return s;
--- idefixGitTraining/src/bibliotek80/BogEksemplar.java
+++ idefixGitTraining/src/bibliotek80/BogEksemplar.java
@@ -1,21 +1,40 @@
 package bibliotek80;
 
+/**
+ *  @author Nurudin, Glenn, Martin, Janus
+ *
+ */
+
 public class BogEksemplar {
 
 private int eksplNr;
 
+/**
+ * Tom constructor
+ */
 public BogEksemplar() {
 	
 }
 
+/**
+ * Constructor der opretter et objekt af typen BogEksemplar
+ * @param eksplNr
+ */
 public BogEksemplar(int eksplNr) {
 	this.eksplNr = eksplNr;
 }
 
+/**
+ * returnere eksplNr attributten
+ * @return
+ */
 public int getEksemplar() {
 	return this.eksplNr;
 }
 
+/**
+ * toString metode der udskriver eksplNr attributten
+ */
 public String toString() {
 	String s = eksplNr + "";
 	
